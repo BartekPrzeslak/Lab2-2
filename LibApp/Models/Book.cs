096b5ebc1b0f040c20554ee757ff3d3852a6cd37
Bartłomiej Prześlak <bartlomiej.przeslak@microsoft.wsei.edu.pl>
2024-02-18 19:57:56 +0100

    Zadanie 1

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -5,16 +5,19 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "tytul jest wymagany")]
+        [StringLength(100, ErrorMessage = "tytul nie może być dluzszy niz 100znakow")]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "autor jest wymagany")]
         public string Author { get; set; }
-        [Required]
+        [Required(ErrorMessage = "gatunek jest wymagany")]
         public Genre Genre { get; set; }
         public int GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "data wydania jest wymagana")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Liczba sztuk jest wymagana")]
+        [Range(1, 20, ErrorMessage = "Liczba sztuk musi byc w przedziale od 1 do 20")]
         public byte NumberInStock { get; set; }
     }
 }
